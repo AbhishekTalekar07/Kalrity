9c054d7e9093c4b95e79a0c48112847a79478b84
Abhishek <Abhishektalekar1@gmail.com>
2024-03-18 11:35:52 +0530

    first commit

---
+++ force-app/main/default/lwc/trial1/trial1.js
@@ -0,0 +1,26 @@
+import { LightningElement , wire, track} from 'lwc';
+import getContact from '@salesforce/apex/TrialPurposeClass.getContact';
+export default class Trial1 extends LightningElement {
+    @track conList;
+    @track contactOptions = [];
+    @track selectedContactId;
+
+    @wire(getContact)
+    wiredContacts({ error, data }) {
+        if (data) {
+            this.conList = data;
+            this.contactOptions = this.conList.map(contact => ({
+                label: contact.FirstName,
+                value: contact.Id
+            }));
+        } else if (error) {
+            console.error('Error retrieving data:', error); 
+        }
+    }
+
+    handleContacts(event) {
+        const selectedContactId = event.detail.value;
+        console.log('Selected Contact Id:', selectedContactId);
+        this.selectedContactId = selectedContactId; 
+    }
+}
